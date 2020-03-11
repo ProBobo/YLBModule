87ac19386d2ebaed72a8c11023ada542b7e4cdcf
yulibo <libo.yu@quvideo.com>
2020-03-11 14:22:49 +0800

    Initial commit

---
+++ Example/YLBModule/YLBViewController.h
@@ -0,0 +1,13 @@
+//
+//  YLBViewController.h
+//  YLBModule
+//
+//  Created by ProBobo on 03/11/2020.
+//  Copyright (c) 2020 ProBobo. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YLBViewController : UIViewController
+
+@end
---
+++ Example/YLBModule/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  YLBModule
+//
+//  Created by ProBobo on 03/11/2020.
+//  Copyright (c) 2020 ProBobo. All rights reserved.
+//
+
+@import UIKit;
+#import "YLBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YLBAppDelegate class]));
+    }
+}
