bda01c15520b928c7416eda1046bbe077ea328c1
yulibo <564885081@qq.com>
2020-03-11 16:32:09 +0800

    Initial commit

---
+++ Example/YLBModule/YLBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YLBAppDelegate.h
+//  YLBModule
+//
+//  Created by ProBobo on 03/11/2020.
+//  Copyright (c) 2020 ProBobo. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YLBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/YLBModule/YLBViewController.m
@@ -0,0 +1,29 @@
+//
+//  YLBViewController.m
+//  YLBModule
+//
+//  Created by ProBobo on 03/11/2020.
+//  Copyright (c) 2020 ProBobo. All rights reserved.
+//
+
+#import "YLBViewController.h"
+
+@interface YLBViewController ()
+
+@end
+
+@implementation YLBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
